b1a67afef8799133d6692c278869cb865e5f4008
Sang Shin <sangshinpassion@gmail.com>
2022-10-31 08:17:10 -0400

    change

--- src/main/java/word_counting_using_only_ArrayList/Main.java
+++ src/main/java/word_counting_using_only_ArrayList/Main.java
@@ -11,14 +11,12 @@ public class Main {
         String initialSentence
                 = "Hello I love Boston. Yes, you do Love Boston.";
         String[] initialWordsInArray = initialSentence.split("[, ?.@]+");
-        List<String> initialWords =
-                new ArrayList<String>(Arrays.asList(initialWordsInArray));
+        List<String> initialWords = Arrays.asList(initialWordsInArray);
 
         List<String> uniqueWords = new ArrayList<>();
         List<Integer> counters = new ArrayList<>();
 
-        int currentIndex = 0;
-        for (String word: initialWords){
+        for (String word : initialWords) {
 
             int foundIndex = isThisWordAlreadyFound(
                     uniqueWords,
@@ -26,18 +24,21 @@ public class Main {
 
             // if the word has been already encountered
             if (foundIndex >= 0) {
-                counters.set(foundIndex, counters.get(foundIndex)+1);
+                counters.set(foundIndex, counters.get(foundIndex) + 1);
             }
             // else - this is the first time the word is encountered
             else {
                 int index = uniqueWords.size();
-                uniqueWords.add(index,  word);
-                counters.add(index, 0);
+                uniqueWords.add(index, word.toLowerCase());
+                counters.add(index, 1);
             }
         }
 
         // display the result
-        System.out.println(uniqueWords);
+        for (int i = 0; i < uniqueWords.size(); i++) {
+            System.out.println(uniqueWords.get(i) + " "
+                    + counters.get(i));
+        }
 
     }
 
@@ -45,6 +46,6 @@ public class Main {
     // otherwise return the index in the uniqueWords array
     private static int isThisWordAlreadyFound(
             List<String> uniqueWords, String word) {
-        return uniqueWords.indexOf(word);
+        return uniqueWords.indexOf(word.toLowerCase());
     }
 }
