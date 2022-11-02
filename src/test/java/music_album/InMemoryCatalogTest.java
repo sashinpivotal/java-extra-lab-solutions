30f1988bb9b443828c547986d5b2f7214a255052
Sang Shin <sangshinpassion@gmail.com>
2022-11-02 16:22:59 -0400

    change

--- src/test/java/music_album/InMemoryCatalogTest.java
+++ src/test/java/music_album/InMemoryCatalogTest.java
@@ -1,19 +1,36 @@
 package music_album;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
 public class InMemoryCatalogTest {
 
-    @Test
-    public void findById() {
+    private InMemoryCatalog inMemoryCatalog;
+    private MusicItem expectedMusicItem;
 
+    @Before
+    public void setUp() throws Exception {
         // arrange
         // - what is test target object?
         // - you have to create an object instance of that test target
-        InMemoryCatalog inMemoryCatalog = new InMemoryCatalog();
+        inMemoryCatalog = new InMemoryCatalog();
+
+        expectedMusicItem = new MusicItem(1L,
+        "Diva",
+        "Annie Lennox",
+        "1992-01-04",
+        13.99,
+        MusicCategory.POP);
+    }
+
+    @Test
+    public void findById() {
 
         // act
         // - what is test target method?
@@ -24,17 +41,21 @@ public class InMemoryCatalogTest {
         // assert
         // - create an expected value - expected MusicItem object
         // - compare expected value with the actual
-        MusicItem expectedMusicItem
-                = new MusicItem(1L,
-                "Diva",
-                "Annie Lennox",
-                "1992-01-04",
-                13.99,
-                MusicCategory.POP);
         Assert.assertEquals(expectedMusicItem, actualMusicItem);
     }
 
     @Test
     public void findByKeyword() {
+
+        // act
+        // - call your test target method with test argument values
+        Collection<MusicItem> actualMusicItems = inMemoryCatalog.findByKeyword("Diva");
+
+        // assert
+        // - create expected value or object
+        // - assert to compare actual against expected
+        ArrayList<MusicItem> expectedMusicItems = new ArrayList<>();
+        expectedMusicItems.add(expectedMusicItem);
+        Assert.assertEquals(expectedMusicItems, actualMusicItems);
     }
 }
